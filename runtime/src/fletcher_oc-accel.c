10ede69d20107121f28aa211bb47bb731d88519b
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2021-10-09 20:36:12 +0200

    Updated library name to fletcher_oc-accel, to finish the move from SNAP.
    The memory mapped register layout had already been changed in earlier commits, effectively breaking compatibility between fletcher's snap and oc-accel platforms.

--- runtime/src/fletcher_oc-accel.c
+++ runtime/src/fletcher_oc-accel.c
@@ -21,7 +21,7 @@
 #include <osnap_global_regs.h>
 
 #include "fletcher/fletcher.h"
-#include "fletcher_snap.h"
+#include "fletcher_oc-accel.h"
 
 da_t buffer_ptr = 0x0;
 
@@ -37,17 +37,17 @@ fstatus_t platformGetName(char *name, size_t size) {
 }
 
 fstatus_t platformInit(void *arg) {
-  debug_print("[FLETCHER_SNAP] Initializing platform.       Arguments @ [host] %016lX.\n", (unsigned long) arg);
+  debug_print("[FLETCHER_OCXL] Initializing platform.       Arguments @ [host] %016lX.\n", (unsigned long) arg);
   // Check psl_server.dat is present
 
   if (snap_state.sim) {
-    debug_print("[FLETCHER_SNAP] Simulation mode.\n");
+    debug_print("[FLETCHER_OCXL] Simulation mode.\n");
     if (access("ocse_server.dat", F_OK) == -1) {
-      debug_print("[FLETCHER_SNAP] No ocse_server.dat file present in working directory. Entering error state.\n");
+      debug_print("[FLETCHER_OCXL] No ocse_server.dat file present in working directory. Entering error state.\n");
       snap_state.error = 1;
       return FLETCHER_STATUS_ERROR;
     }
-    debug_print("[FLETCHER_SNAP] ocse_server.dat present.\n");
+    debug_print("[FLETCHER_OCXL] ocse_server.dat present.\n");
   }
 
   if(snap_state.card_no == 0)
@@ -58,7 +58,7 @@ fstatus_t platformInit(void *arg) {
   snap_state.card_handle = snap_card_alloc_dev(snap_state.device, SNAP_VENDOR_ID_IBM, SNAP_DEVICE_ID_SNAP);
 
   if (snap_state.card_handle == NULL) {
-    debug_print("[FLETCHER_SNAP] Could not allocate SNAP card. Entering error state.");
+    debug_print("[FLETCHER_OCXL] Could not allocate SNAP card. Entering error state.");
     snap_state.error = 1;
     return FLETCHER_STATUS_ERROR;
   }
@@ -67,7 +67,7 @@ fstatus_t platformInit(void *arg) {
 
   snap_card_ioctl(snap_state.card_handle, GET_CARD_TYPE, (unsigned long) &ioctl_data);
 
-  debug_print("[FLETCHER_SNAP] Card: ");
+  debug_print("[FLETCHER_OCXL] Card: ");
   switch (ioctl_data) {
     case 0:debug_print("ADKU3\n");
       break;
@@ -80,26 +80,26 @@ fstatus_t platformInit(void *arg) {
   }
 
   snap_card_ioctl(snap_state.card_handle, GET_SDRAM_SIZE, (unsigned long) &ioctl_data);
-  debug_print("[FLETCHER_SNAP] Available card RAM: %d\n", (int) ioctl_data);
+  debug_print("[FLETCHER_OCXL] Available card RAM: %d\n", (int) ioctl_data);
 
   snap_action_flag_t attach_flags = (snap_action_flag_t) 0;
 
   snap_state.action_handle = snap_attach_action(snap_state.card_handle, snap_state.action_type, attach_flags, 100);
 
   if (snap_state.action_handle == NULL) {
-	  debug_print("[FLETCHER_SNAP] Could not attach action. Entering error state.");
+	  debug_print("[FLETCHER_OCXL] Could not attach action. Entering error state.");
 	      snap_state.error = 1;
 	      return FLETCHER_STATUS_ERROR;
   }
 
-  debug_print("[FLETCHER_SNAP] Action attached.\n");
+  debug_print("[FLETCHER_OCXL] Action attached.\n");
 
   return FLETCHER_STATUS_OK;
 }
 
 fstatus_t platformWriteMMIO(uint64_t offset, uint32_t value) {
-  snap_action_write32(snap_state.card_handle, FLETCHER_SNAP_ACTION_REG_OFFSET + 4*offset, value);
-  debug_print("[FLETCHER_SNAP] Writing MMIO register.       %04lu <= 0x%08X\n", offset, value);
+  snap_action_write32(snap_state.card_handle, FLETCHER_OCXL_ACTION_REG_OFFSET + 4*offset, value);
+  debug_print("[FLETCHER_OCXL] Writing MMIO register.       %04lu <= 0x%08X\n", offset, value);
   return FLETCHER_STATUS_OK;
 }
 
@@ -109,18 +109,18 @@ fstatus_t platformReadMMIO(uint64_t offset, uint32_t *value) {
   if (snap_state.sim && offset == FLETCHER_REG_STATUS) {
     sleep(2);
   }
-  int rc = snap_action_read32(snap_state.card_handle, FLETCHER_SNAP_ACTION_REG_OFFSET + 4*offset, value);
+  int rc = snap_action_read32(snap_state.card_handle, FLETCHER_OCXL_ACTION_REG_OFFSET + 4*offset, value);
   if (rc != SNAP_OK) {
-    fprintf(stderr, "[FLETCHER_SNAP] Error while reading MMIO register: %d\n", rc);
+    fprintf(stderr, "[FLETCHER_OCXL] Error while reading MMIO register: %d\n", rc);
     return FLETCHER_STATUS_ERROR;
   }
-  debug_print("[FLETCHER_SNAP] Reading MMIO register.       %04lu => 0x%08X\n", offset, *value);
+  debug_print("[FLETCHER_OCXL] Reading MMIO register.       %04lu => 0x%08X\n", offset, *value);
   return FLETCHER_STATUS_OK;
 }
 
 fstatus_t platformCopyHostToDevice(const uint8_t *host_source, da_t device_destination, int64_t size) {
   debug_print(
-      "[FLETCHER_SNAP] Copying from host to device. [host] 0x%016lX --> [dev] 0x%016lX (%lu bytes) (NOT IMPLEMENTED)\n",
+      "[FLETCHER_OCXL] Copying from host to device. [host] 0x%016lX --> [dev] 0x%016lX (%lu bytes) (NOT IMPLEMENTED)\n",
       (uint64_t) host_source,
       device_destination,
       size);
@@ -129,7 +129,7 @@ fstatus_t platformCopyHostToDevice(const uint8_t *host_source, da_t device_desti
 
 fstatus_t platformCopyDeviceToHost(da_t device_source, uint8_t *host_destination, int64_t size) {
   debug_print(
-      "[FLETCHER_SNAP] Copying from device to host. [dev] 0x%016lX --> [host] 0x%016lX (%lu bytes) (NOT IMPLEMENTED)\n",
+      "[FLETCHER_OCXL] Copying from device to host. [dev] 0x%016lX --> [host] 0x%016lX (%lu bytes) (NOT IMPLEMENTED)\n",
       device_source,
       (uint64_t) host_destination,
       size);
@@ -137,7 +137,7 @@ fstatus_t platformCopyDeviceToHost(da_t device_source, uint8_t *host_destination
 }
 
 fstatus_t platformTerminate(void *arg) {
-  debug_print("[FLETCHER SNAP] Terminating platform.        Arguments @ [host] 0x%016lX.\n", (uint64_t) arg);
+  debug_print("[FLETCHER_OCXL] Terminating platform.        Arguments @ [host] 0x%016lX.\n", (uint64_t) arg);
   snap_detach_action(snap_state.action_handle);
   snap_card_free(snap_state.card_handle);
   return FLETCHER_STATUS_OK;
@@ -145,8 +145,8 @@ fstatus_t platformTerminate(void *arg) {
 
 fstatus_t platformDeviceMalloc(da_t *device_address, int64_t size) {
   da_t ptr;
-  posix_memalign((void**)(&ptr), FLETCHER_SNAP_DEVICE_ALIGNMENT, size);
-  debug_print("[FLETCHER_SNAP] Allocating device memory.    [device] 0x%016lX (%10lu bytes).\n",
+  posix_memalign((void**)(&ptr), FLETCHER_OCXL_DEVICE_ALIGNMENT, size);
+  debug_print("[FLETCHER_OCXL] Allocating device memory.    [device] 0x%016lX (%10lu bytes).\n",
                ptr,
                size);
   *device_address = ptr;
@@ -154,14 +154,14 @@ fstatus_t platformDeviceMalloc(da_t *device_address, int64_t size) {
 }
 
 fstatus_t platformDeviceFree(da_t device_address) {
-  debug_print("[FLETCHER_SNAP] Freeing device memory.       [device] 0x%016lX. (NOT IMPLEMENTED)\n", device_address);
+  debug_print("[FLETCHER_OCXL] Freeing device memory.       [device] 0x%016lX. (NOT IMPLEMENTED)\n", device_address);
   return FLETCHER_STATUS_OK;
 }
 
 fstatus_t platformPrepareHostBuffer(const uint8_t *host_source, da_t *device_destination, int64_t size, int *alloced) {
   *device_destination = (da_t) host_source;
   *alloced = 0;
-  debug_print("[FLETCHER_SNAP] Preparing buffer for device. [host] 0x%016lX --> 0x%016lX (%10lu bytes).\n",
+  debug_print("[FLETCHER_OCXL] Preparing buffer for device. [host] 0x%016lX --> 0x%016lX (%10lu bytes).\n",
               (unsigned long) host_source,
               (unsigned long) *device_destination,
               size);
@@ -172,7 +172,7 @@ fstatus_t platformPrepareHostBuffer(const uint8_t *host_source, da_t *device_des
 fstatus_t platformCacheHostBuffer(const uint8_t *host_source, da_t *device_destination, int64_t size) {
   *device_destination = buffer_ptr;
   debug_print(
-      "[FLETCHER_SNAP] Caching buffer on device.    [host] 0x%016lX --> 0x%016lX (%10lu bytes). (NOT IMPLEMENTED)\n",
+      "[FLETCHER_OCXL] Caching buffer on device.    [host] 0x%016lX --> 0x%016lX (%10lu bytes). (NOT IMPLEMENTED)\n",
       (unsigned long) host_source,
       (unsigned long) *device_destination,
       size);
