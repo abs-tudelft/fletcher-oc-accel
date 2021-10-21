cda5caa8a40c191a1efcacce347afb7bea99d8c0
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2021-10-21 11:37:29 +0200

    Removed system-specific line from the new notebook.
    If a python environment is needed to run the notebook, it needs to be sourced outside of the script.

--- examples/sum/sw/run_ocxl_cosim_notebook.sh
+++ examples/sum/sw/run_ocxl_cosim_notebook.sh
@@ -4,5 +4,4 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cp $scriptdir/sum.ipynb ./
 cp $scriptdir/../hw/sum/recordbatch.rb ./
-source ~/python_venvs/fletcher_updated/bin/activate
 python -m notebook ./sum.ipynb
