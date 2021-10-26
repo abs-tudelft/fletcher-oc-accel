b0929f0922a32b2bef1b5becd38dc46dea2e47d3
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2021-10-26 16:46:05 +0200

    Using jupyter-notebook to open the notebook

--- examples/sum/sw/run_ocxl_cosim_notebook.sh
+++ examples/sum/sw/run_ocxl_cosim_notebook.sh
@@ -4,4 +4,4 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cp $scriptdir/sum.ipynb ./
 cp $scriptdir/../hw/sum/recordbatch.rb ./
-python -m notebook ./sum.ipynb
+jupyter-notebook ./sum.ipynb
